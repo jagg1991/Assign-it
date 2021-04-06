3715c6dba95e6d7f49160e15c3883f53126fa50d
jagg1991 <josephguajardo@ymail.com>
2021-04-05 18:48:15 -0700

    adding files

--- models/user.js
+++ models/user.js
@@ -1,7 +1,11 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class User extends Model { }
+class User extends Model {
+    // checkPassword(loginPw) {
+    //     return bcrypt.compareSync(loginPw, this.password);
+    // }
+}
 
 User.init({
     id: {
@@ -40,6 +44,16 @@ User.init({
     // manager_id:
 },
     {
+        // hooks: {
+        //     beforeCreate: async (newUserData) => {
+        //         newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        //         return newUserData;
+        //     },
+        //     beforeUpdate: async (updatedUserData) => {
+        //         updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        //         return updatedUserData;
+        //     },
+        // },
         sequelize,
         timestamps: false,
         freezeTableName: true,
--- public/js/login.js
+++ public/js/login.js
@@ -1,33 +1,6 @@
-// const signupFormHandler = async (event) => {
-//     event.preventDefault();
 
-//     const name = document.querySelector('#name-signup').value.trim();
-//     const email = document.querySelector('#email-signup').value.trim();
-//     const password = document.querySelector('#password-signup').value.trim();
-//     const role = document.querySelector('.form-select').value;
-//     console.log(role)
 
-//     if (name && email && password && role) {
-//         console.log('sucess')
-//         const response = await fetch('/api/user', {
-//             method: 'POST',
-//             body: JSON.stringify({ name, email, password, role }),
-//             headers: { 'Content-Type': 'application/json' },
-//         });
-
-//         if (response.ok) {
-//             document.location.replace('/home');
-//         } else {
-//             alert(response.statusText);
-//         }
-//     }
-// };
-
-// document
-//     .querySelector('.signup-form')
-//     .addEventListener('submit', signupFormHandler);
-
-const signupFormHandler = async (event) => {
+const loginFormHandler = async (event) => {
     event.preventDefault();
 
     const user = document.querySelector('#email-login').value.trim();
@@ -36,17 +9,21 @@ const signupFormHandler = async (event) => {
     if (user && password) {
         // Send a POST request to the API endpoint
         const response = await fetch('/api/user/login', {
-            method: 'POST',
-            body: JSON.stringify({ email, password }),
+            method: 'GET',
+            body: JSON.stringify({ user, password }),
             headers: { 'Content-Type': 'application/json' },
         });
 
         if (response.ok) {
             // If successful, redirect the browser to the profile page
-            document.location.replace('/manager');
+            document.location.replace('/home');
         } else {
             alert(response.statusText);
         }
     }
 
 };
+
+document
+    .querySelector('.login-form')
+    .addEventListener('submit', loginFormHandler);
